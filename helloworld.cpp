17da13291f5817edd35764c5491db94568f423c4
Deedikjepijn <joris8393@gmail.com>
2023-02-03 14:21:06 +0100

    Removed lots of double code!

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,35 +6,23 @@ int main(){
     cout << "what do you want to do? Your options are + - and x: ";
     char x;
     cin >> x;
-    if (x == '+'){
-        cout << "you chose +\n" << endl;
-        cout << "Type in the first numer: ";
+    if (x == '+' || x == '-' || x == 'x' || x == 'X' || x == '*') {
+        cout << "you chose " << x <<"\n" << endl;
+        cout << "Type in the first number: ";
         int a;
         cin >> a;
         cout << "Type in the second number: ";
         int b;
-        cin >> b;
-        cout << "The sum of " << a << " and " << b << " is " << a + b << endl;
-    }
-    else if (x == '-'){
-        cout << "you chose -\n" << endl;
-        cout << "Type in the first numer: ";
-        int a;
-        cin >> a;
-        cout << "Type in the second number: ";
-        int b;
-        cin >> b;
-        cout << "The awnser with the sum is: " << a << " and " << b << " is " << a - b << endl;
-    }
-    else if (x == 'x' || x == 'X' || x == '*'){
-        cout << "you chose x\n" << endl;
-        cout << "Type in the first numer: ";
-        int a;
-        cin >> a;
-        cout << "Type in the second number: ";
-        int b;
-        cin >> b;
-        cout << "The awnser with the sum is: " << a << " and " << b << " is " << a * b << endl;
+        cin >> b;    
+        if (x == '+'){
+            cout << "The sum of " << a << " and " << b << " is " << a + b << endl;
+        }
+        else if (x == '-'){
+            cout << "The awnser with the sum is: " << a << " and " << b << " is " << a - b << endl;
+        }
+        else if (x == 'x' || x == 'X' || x == '*'){
+            cout << "The awnser with the sum is: " << a << " and " << b << " is " << a * b << endl;
+        }
     }
     else{
         cout << "That is no valid option, the program will be closed now\n" << endl;
