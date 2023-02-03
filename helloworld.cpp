ad1c0abe7e7735ef321a1b60acdfa9ea3f8f8ab6
Deedikjepijn <joris8393@gmail.com>
2023-02-03 13:32:18 +0100

    added second line

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
-} 
+int main()
+{
+    cout << "Hello word! \n";
+    cout << "random tekst go brrr";
+    return 0;
+}
