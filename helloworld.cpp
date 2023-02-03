573425b783897cecce1f43e64bec689e6179f77d
Deedikjepijn <joris8393@gmail.com>
2023-02-03 13:35:39 +0100

    added number input

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main()
 {
     cout << "Hello word! \n";
-    cout << "random tekst go brrr";
+    cout << "random tekst go brrr \n";
+    cout << "even more random tekst \n";
+    int x;
+    cout << "type a random number: ";
+    cin >> x;
+    cout << "your number is: " << x;
     return 0;
 }
